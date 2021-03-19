b2e976eb53d1e28b791f1da7995eb509390b1c0a
Ivailo Atanasov <60134259+ivoqqq@users.noreply.github.com>
2021-03-19 09:29:07 +0200

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -6,15 +6,5 @@ module.exports = {
                 args[0].title = "Less Known Places";
                 return args;
             })
-    },
-    // css: {
-    //     sourceMap: true,
-    //     loaderOptions: {
-    //         sass: {
-    //             data: `
-    //           @import "./styles/container.scss";
-    //         `,
-    //         },
-    //     },
-    // },
+    }
 }
